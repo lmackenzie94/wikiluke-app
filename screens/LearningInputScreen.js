0a2426d748c6d7e8154034095c93f00caf802a17
Luke MacKenzie <mackenzieluke94@gmail.com>
2020-06-18 14:02:27 -0400

    added learning category

--- screens/LearningInputScreen.js
+++ screens/LearningInputScreen.js
@@ -6,7 +6,14 @@ import Button from '../components/Button';
 import Colours from '../constants/colours';
 import doFetch from '../utils/doFetch';
 
-const categories = [`Misc`, `Business`, `Tech`, `Science`, `Psychology`];
+const categories = [
+  `Misc`,
+  `Business`,
+  `Tech`,
+  `Science`,
+  `Psychology`,
+  `Food & Health`,
+];
 
 const LearningInputScreen = () => {
   const [learning, setLearning] = useState('');
